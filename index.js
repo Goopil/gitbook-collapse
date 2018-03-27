4b7eb4e88b1676b468f8631ed74a1a61ff394717
Zachary Volpi <z.volpi@infomir.com>
2018-03-27 09:20:03 +0200

    render markdown in block

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ module.exports = {
       process: function (blk) {
         const title = blk.kwargs.title || '';
         const color = blk.kwargs.color || '';
-        return `
+
+        return this.renderBlock('markdown', blk.body).then(str => {
+          return `
 <div class="wrap-collabsible">
     <input id="collapsible" class="toggle" type="checkbox">
     <label for="collapsible" class="lbl-toggle ${color}">${title}</label>
     <div class="collapsible-content">
-        <div class="content-inner">${blk.body}</div>
+        <div class="content-inner">${str}</div>
     </div>
 </div>`;
+        })
       }
     }
   }
