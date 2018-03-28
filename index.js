4a102b9c4118c64f26dffc4214aaa398705314be
Zachary Volpi <z.volpi@infomir.com>
2018-03-28 10:28:20 +0200

    fix params call bug

--- index.js
+++ index.js
@@ -14,16 +14,18 @@ module.exports = {
     collapse: {
       process: function (blk) {
         const processMarkDown = blk.kwargs.process !== false
-        const title = blk.kwargs.title !== ''
+        const title = blk.kwargs.title || ''
+        const title = blk.kwargs.title || ''
 
         function format(body, summary) {
-          const summary = sumary || ''
-          const body = body || ''
+          summary = summary || ''
+          body = body || ''
+
           return '<details><summary>' + summary + '</summary>' + body + '</details>';
         }
 
         return processMarkDown === false ?
-          format(body, title) :
+          format(blk.body, title) :
           this.renderBlock('markdown', blk.body)
           .then(function (data) {
             return format(data, title)
